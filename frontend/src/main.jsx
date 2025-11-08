8689ef3ac61259b7c87fd342792f038e37aa145f
duy098357 <duy098357@gmail.com>
2025-11-08 16:32:04 -0600

    back script

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,8 @@ import ReactDOM from "react-dom/client";
 import Chatbot from "./App";
 import "./index.css"; // tailwind or your css
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <Chatbot />
-//   </React.StrictMode>
-// );
-
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Chatbot />
+  </React.StrictMode>
+);
