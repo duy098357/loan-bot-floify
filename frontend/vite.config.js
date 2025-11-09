e38662d0e18d0bdd6d0011c4b3b13b6d03d827e6
duy098357 <duy098357@gmail.com>
2025-11-09 11:57:53 -0600

    vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-export default defineConfig({
-  plugins: [react()],
-  server: { port: 5173 },
-  build: { outDir: "dist" }
-});
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// export default defineConfig({
+//   plugins: [react()],
+//   server: { port: 5173 },
+//   build: { outDir: "dist" }
+// });
+
+export default {
+  build: {
+    lib: {
+      entry: './src/widget.js', // your entry point for embed script
+      name: 'LoanChatWidget',
+      fileName: 'widget',
+      formats: ['iife']
+    },
+    rollupOptions: {
+      external: [], // no external dependencies
+    },
+  },
+};
