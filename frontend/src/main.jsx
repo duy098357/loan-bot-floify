2ef740f423ee6b6e784e1088d6b1e6bb63003333
duy098357 <duy098357@gmail.com>
2025-11-09 12:20:29 -0600

    backup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import ChatWidget from "./components/ChatWidget";
-// import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ChatWidget from "./components/ChatWidget";
+import "./index.css";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <ChatWidget />
-//   </React.StrictMode>
-// );
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChatWidget />
+  </React.StrictMode>
+);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,9 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// export default defineConfig({
-//   plugins: [react()],
-//   server: { port: 5173 },
-//   build: { outDir: "dist" }
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
-  build: {
-    lib: {
-            entry: "src/widget.jsx",
-            name: "LoanChatWidget",
-            fileName: "widget",
-            formats: ["iife"],
-          },
-
-  },
+  server: { port: 5173 },
+  build: { outDir: "dist" }
 });
+
+
